8a1b22828b05bdbba4898f85dfd835490b159feb
YarPodl <yar.podlesnov@yandex.ru>
2018-02-16 22:36:23 +0300

    Готово

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -30,46 +30,50 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(39, 279);
+            this.button1.Location = new System.Drawing.Point(276, 188);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.Size = new System.Drawing.Size(133, 27);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Генерировать";
+            this.button1.Text = "Показать элементы";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // listView1
             // 
-            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listView1.Location = new System.Drawing.Point(26, 12);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(156, 180);
+            this.listView1.Size = new System.Drawing.Size(465, 146);
             this.listView1.TabIndex = 1;
             this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.View = System.Windows.Forms.View.List;
             // 
-            // listBox1
+            // button2
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(194, 12);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(144, 420);
-            this.listBox1.TabIndex = 2;
+            this.button2.Location = new System.Drawing.Point(74, 188);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 27);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Посчитать время доступа";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(503, 437);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(503, 242);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Лабораторная работа №3.  Использование коллекций";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -78,7 +82,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ namespace lab3
     {
         const int length = 100000;
         Queue<APS> collect = new Queue<APS>(length);
+        APS[] array = new APS[length];
         public Form1()
         {
             InitializeComponent();
@@ -22,15 +24,77 @@ namespace lab3
 
         private void button1_Click(object sender, EventArgs e)
         {
-            collect.Clear();
-            for (int i = 0; i < length; i++)
-            {
-                collect.Enqueue(new APS("name" + i.ToString(), i));
-            }
             foreach (APS item in collect)
             {
+                listView1.Items.Clear();
                 listView1.Items.Add(item.ToStringPart());
-                //listBox1.Items.Add(item.ToStringPart());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            Stopwatch sWatch = new Stopwatch();
+            sWatch.Start();
+            for (int i = 0; i < length; i++)
+            {
+                array[i].freeLines = i;
+            }
+            sWatch.Stop();
+            listView1.Items.Add("Время последовательного доступа к массиву: "
+                + sWatch.ElapsedTicks);
+            
+            Random random = new Random();
+            sWatch.Restart();
+            for (int i = 0; i < length; i++)
+            {
+                array[random.Next(length)].countUsers = i;
+            }
+            sWatch.Stop();
+            listView1.Items.Add("Время произвольного доступа к массиву: "
+                + sWatch.ElapsedTicks);
+
+            sWatch.Restart();
+            for (int i = 0; i < length; i++)
+            {
+                APS temp = collect.Dequeue();
+                temp.tarif = "Тариф";
+                collect.Enqueue(temp);
+            }
+            sWatch.Stop();
+            listView1.Items.Add("Время последовательного доступа к очереди: "
+                + sWatch.ElapsedTicks);
+
+            Queue<APS> tempQueue = new Queue<APS>(length);
+            sWatch.Restart();
+            for (int i = 0; i < length; i++)
+            {
+                int n = random.Next(length);
+                for (int j = 0; j < n; j++)
+                {
+                    tempQueue.Enqueue(collect.Dequeue());
+                }
+                APS temp = collect.Dequeue();
+                temp.usersPay = i;
+                tempQueue.Enqueue(temp);
+                for (int j = n + 1; j < length; j++)
+                {
+                    tempQueue.Enqueue(collect.Dequeue());
+                }
+                collect = tempQueue;
+            }
+            sWatch.Stop();
+            listView1.Items.Add("Время произвольного доступа к очереди: "
+                + sWatch.ElapsedTicks);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                APS newObject = new APS("name" + i.ToString(), i);
+                collect.Enqueue(newObject);
+                array[i] = newObject;
             }
         }
     }
